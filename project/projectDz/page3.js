f2fe96bb14bab25bd947affe0ff9762b8b7b1b43
Светлана <fox.2025.owl@gmail.com>
2025-09-20 21:03:57 +1000

    dz создание трёх страниц

---
+++ project/projectDz/page3.js
@@ -0,0 +1,14 @@
+document.getElementById("form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    const answer = document.querySelector('input[name="answer"]:checked');
+    if (!answer) return alert("Please select an answer");
+    const answers = JSON.parse(localStorage.getItem("questionAnswers") || "[]");
+    answers[2] = answer.value;
+    localStorage.setItem("questionAnswers", JSON.stringify(answers));
+    const resultsDiv = document.getElementById("results");
+        resultsDiv.innerHTML = `
+        <p><strong>Q1</strong> Do you like Javascript? -> ${answers[0] || "Not answered"}</p>
+        <p><strong>Q1</strong> Do you use TailwindCSS? -> ${answers[1] || "Not answered"}</p>
+        <p><strong>Q1</strong> Do you enjoy debugging? -> ${answers[2]}</p>
+        `;
+});
